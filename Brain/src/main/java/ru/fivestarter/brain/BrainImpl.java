46ee08028c37b07943de3092bcf881dd0253fdb2
ystartsev <ystartsev@wiley.com>
2014-12-11 18:07:51 +0300

    neyrons were added

--- Brain/src/main/java/ru/fivestarter/brain/BrainImpl.java
+++ Brain/src/main/java/ru/fivestarter/brain/BrainImpl.java
@@ -1,7 +1,12 @@
 package ru.fivestarter.brain;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 import ru.fivestarter.brain.neyron.Effector;
 import ru.fivestarter.brain.neyron.Neyron;
+import ru.fivestarter.brain.neyron.NeyronImpl;
 import ru.fivestarter.brain.neyron.Sensor;
 import ru.fivestarter.brain.observer.Observer;
 
@@ -10,13 +15,19 @@ import ru.fivestarter.brain.observer.Observer;
  * @version 11.12.2014
  */
 public class BrainImpl implements Brain, Observer {
-    Neyron sensor;
-    Neyron effector;
+    private static final int NEYRONS_NUMBER = 10;
+
+    private Neyron sensor;
+    private Neyron effector;
+    private List<Neyron> neyronList;
+    private SynapseManager synapseManager = new SynapseManagerImpl();
 
     public void init() {
         sensor = new Sensor();
+        sensor.registerObserver((Observer) synapseManager);
         effector = new Effector();
         effector.registerObserver(this);
+        initNeyronList((Observer) synapseManager);
     }
 
     @Override
@@ -32,5 +43,14 @@ public class BrainImpl implements Brain, Observer {
     @Override
     public void update() {
         action();
+    }
+
+    private void initNeyronList(Observer observer) {
+        neyronList = Lists.newArrayList();
+        for (int i = 0; i < NEYRONS_NUMBER; i++) {
+            Neyron neyron = new NeyronImpl();
+            neyron.registerObserver(observer);
+            neyronList.add(neyron);
+        }
     }
 }
--- Brain/src/main/java/ru/fivestarter/brain/SynapseManagerImpl.java
+++ Brain/src/main/java/ru/fivestarter/brain/SynapseManagerImpl.java
@@ -1,8 +1,14 @@
 package ru.fivestarter.brain;
 
+import ru.fivestarter.brain.observer.Observer;
+
 /**
  * @author <a href='mailto:ystartsev@wiley.com'>Yury Startsev</a>
  * @version 11.12.2014
  */
-public class SynapseManagerImpl implements SynapseManager {
+public class SynapseManagerImpl implements SynapseManager, Observer {
+    @Override
+    public void update() {
+
+    }
 }
