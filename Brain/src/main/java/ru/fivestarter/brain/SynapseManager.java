aa31594ec2408270fc995d6f2236b122dac9106a
ystartsev <ystartsev@wiley.com>
2014-12-11 15:23:46 +0300

    brain init

---
+++ Brain/src/main/java/ru/fivestarter/brain/SynapseManager.java
@@ -0,0 +1,8 @@
+package ru.fivestarter.brain;
+
+/**
+ * @author <a href='mailto:ystartsev@wiley.com'>Yury Startsev</a>
+ * @version 11.12.2014
+ */
+public interface SynapseManager {
+}
---
+++ Brain/src/main/java/ru/fivestarter/brain/neyron/Neyron.java
@@ -0,0 +1,8 @@
+package ru.fivestarter.brain.neyron;
+
+/**
+ * @author <a href='mailto:ystartsev@wiley.com'>Yury Startsev</a>
+ * @version 11.12.2014
+ */
+public interface Neyron {
+}
